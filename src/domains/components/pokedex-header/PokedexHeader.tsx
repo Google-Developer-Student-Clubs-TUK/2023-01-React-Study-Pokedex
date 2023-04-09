623df27f7be5f93b030ab60f9e162989c8e2ad83
jqkk <kkj2551@naver.com>
2023-04-10 01:10:35 +0900

    :sparkles: feat: PokedexHeader 컴포넌트에 데이터 패칭 로직 반영

--- src/domains/components/pokedex-header/PokedexHeader.tsx
+++ src/domains/components/pokedex-header/PokedexHeader.tsx
@@ -2,17 +2,28 @@ import 'twin.macro';
 
 import React from 'react';
 
-const PokedexHeader = () => (
-  <>
-    <div tw='container flex items-center px-8 mt-8 justify-between mx-auto max-w-5xl'>
-      <p tw='text-2xl opacity-70 font-semibold flex gap-2 items-center'>
-        Pokémon
-      </p>
-      <p tw='text-2xl opacity-70 font-semibold'>111</p>
-    </div>
-    <div tw='container flex items-center px-8 justify-between mx-auto max-w-5xl'>
-      <p tw='text-8xl font-semibold opacity-70 font-fun'>bulbasaur</p>
-    </div>
-  </>
-);
+import { useGetPokemonBasicInfoQuery, useNavigation } from '@/domains/hooks';
+import { createLeadingZero } from '@/domains/utils';
+
+const PokedexHeader = () => {
+  const { currentNumber } = useNavigation();
+  const { data } = useGetPokemonBasicInfoQuery(currentNumber);
+
+  return (
+    <>
+      <div tw='container flex items-center px-8 mt-8 justify-between mx-auto max-w-5xl'>
+        <p tw='text-2xl opacity-70 font-semibold flex gap-2 items-center'>
+          Pokémon
+        </p>
+        <p tw='text-2xl opacity-70 font-semibold'>{`#${createLeadingZero(
+          currentNumber,
+        )}`}</p>
+      </div>
+      <div tw='container flex items-center px-8 justify-between mx-auto max-w-5xl'>
+        <p tw='text-8xl font-semibold opacity-70 font-fun'>{data?.name}</p>
+      </div>
+    </>
+  );
+};
+
 export default PokedexHeader;
