25819fc70f782abf0d284af87a9b4c1382c518e8
jqkk <kkj2551@naver.com>
2023-04-09 16:42:49 +0900

    :sparkles: feat: NextIcon\&PreviousIcon 컴포넌트 추가

---
+++ src/components/icons/PreviousIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const PreviousIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    fill='none'
+    viewBox='0 0 24 24'>
+    <path
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='2'
+      d='M15 18l-6-6 6-6'
+    />
+  </svg>
+);
+
+export default PreviousIcon;
---
+++ src/components/icons/index.ts
@@ -0,0 +1,2 @@
+export { default as NextIcon } from './NextIcon';
+export { default as PreviousIcon } from './PreviousIcon';
