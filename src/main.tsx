93ccb950b884074b8a2cb23a0d925e49b42023e2
jqkk <kkj2551@naver.com>
2023-04-09 17:23:37 +0900

    :sparkles: feat: React-Query\&Reqct-Query-Devtools 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { DevTools } from 'jotai-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -6,10 +8,15 @@ import GlobalStyles from '@/styles/GlobalStyles';
 
 import App from './App';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
-    <DevTools />
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyles />
+      <App />
+      <DevTools />
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
