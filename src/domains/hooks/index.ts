459deffb607e22f07f01aa3a1f9877f9bbedb1cf
jqkk <kkj2551@naver.com>
2023-04-19 01:04:13 +0900

    :truck: chore: Delete useGetPokemonBasicInfoQuery

--- src/domains/hooks/index.ts
+++ src/domains/hooks/index.ts
@@ -1,5 +1,4 @@
 export { default as useGetEvolutionChainQuery } from './queries/useGetEvolutionChainQuery';
-export { default as useGetPokemonBasicInfoQuery } from './queries/useGetPokemonBasicInfoQuery';
 export { default as useGetPokemonInfoQueries } from './queries/useGetPokemonInfoQueries';
 export { default as useGetPokemonStatsQuery } from './queries/useGetPokemonStatsQuery';
 export { default as useBackgroundColor } from './useBackgroundColor';
