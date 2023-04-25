51204b2f13c9b6b7478c8aa0a4754411828e3c51
jqkk <kkj2551@naver.com>
2023-04-26 02:46:02 +0900

    :sparkles: feat: Title 추가

--- src/pages/PokedexListPage.tsx
+++ src/pages/PokedexListPage.tsx
@@ -5,7 +5,8 @@ import React from 'react';
 import PokedexList from '@/domains/components/pokedex-list/PokedexList';
 
 const PokedexListPage = () => (
-  <div tw='w-1/2 mx-auto mt-40'>
+  <div tw='w-1/2 mx-auto mt-10'>
+    <h1 tw='text-center text-7xl font-bold mb-20'>GDSC TUK React Study</h1>
     <PokedexList />
   </div>
 );
