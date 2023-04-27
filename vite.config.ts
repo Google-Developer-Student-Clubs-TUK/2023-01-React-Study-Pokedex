262d8fc92ebf57b7b909a70ea151e41213c71afe
pione3r <dev2diver@gmail.com>
2023-04-27 19:27:55 +0900

    chore: base url 변경

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "/poke-encyclopedia/",
+  base: "/poke-encyclopedia",
   server: {
     port: 3000,
   },
